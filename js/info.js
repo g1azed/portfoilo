314265f1d069f0fab19f60039ee94128fae5e5f9
SUJI <victoria1230120@gmail.com>
2022-12-22 06:00:26 +0900

    aa

--- js/info.js
+++ js/info.js
@@ -17,37 +17,47 @@ $(window).scroll(function () {
 	var scrollValue = $(document).scrollTop();
 	// console.log(scrollValue);
 
-  if(scrollValue >= 280){
-    changeIMG();
+  if(scrollValue >= 380){
+    // changeIMG();
+    $(".back").css({
+      "background":"url('https://user-images.githubusercontent.com/96365042/209000193-0cec16cb-0ff6-41a4-b3de-757a7a502522.png')",
+      "background-size" : "contain",
+      "background-repeat": "no-repeat",
+      "z-index":"0"
+    }); 	
   }else{
-    changeIMG_2();
+    // changeIMG_2();
+  $(".back").css({
+    "background":"url('../img/info/conceptshot/concept_2.png')",
+    "background-size" : "contain",
+    "background-repeat": "no-repeat",	
+      "z-index":"-10"
+    }); 	
   }
+
+    
+
 });
 
 
 // 이미지체인지 ex
-function changeIMG() {
-  //img 태그의 아이디로 변경 할 때
-  // $("#imgID").attr("src", "/data/img2.png");
+// function changeIMG() {
+//   //img 태그의 아이디로 변경 할 때
+//   // $("#imgID").attr("src", "/data/img2.png");
 
-  //img 태그의 클래스 명으로 변경 할 때
-  // $(".imgClass").attr("src", "/data/img2.png");
-  // $(".back").css("background-image", "../img/info/conceptshot/concept_3.png");
-  
-  $(".back").css({
-    "background":"url('../img/info/conceptshot/concept_3.png')",
-    "background-size" : "contain",
-    "background-repeat": "no-repeat"
-  }); 	
+//   //img 태그의 클래스 명으로 변경 할 때
+//   // $(".imgClass").attr("src", "/data/img2.png");
+//   // $(".back").css("background-image", "../img/info/conceptshot/concept_3.png");
 
-}
-function changeIMG_2() {
-  $(".back").css({
-    "background":"url('../img/info/conceptshot/concept_2.png')",
-    "background-size" : "contain",
-    "background-repeat": "no-repeat"
-  }); 	
-}
+
+// }
+// function changeIMG_2() {
+//   $(".back").css({
+//     "background":"url('../img/info/conceptshot/concept_2.png')",
+//     "background-size" : "contain",
+//     "background-repeat": "no-repeat"
+//   }); 	
+// }
 
 
 // 패럴랙스
@@ -58,6 +68,7 @@ $(window).scroll(function (e) {
 function parallax() {
   var scrolled = $(window).scrollTop();
   $(".back").css("background-position-y", (scrolled * 0.8) + "px");
+  $(".back2").css("background-position-y", (scrolled * 0.8) + "px");
 }
 
 // 스크롤 시 백그라운드 이미지 경로 바꾸기
