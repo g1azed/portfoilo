a110c05f8d28e2e24cf08482612bb692282c47bb
SUJI <victoria1230120@gmail.com>
2022-12-22 06:01:24 +0900

    a

--- js/info.js
+++ js/info.js
@@ -17,23 +17,23 @@ $(window).scroll(function () {
 	var scrollValue = $(document).scrollTop();
 	// console.log(scrollValue);
 
-  if(scrollValue >= 380){
-    // changeIMG();
-    $(".back").css({
-      "background":"url('https://user-images.githubusercontent.com/96365042/209000193-0cec16cb-0ff6-41a4-b3de-757a7a502522.png')",
-      "background-size" : "contain",
-      "background-repeat": "no-repeat",
-      "z-index":"0"
-    }); 	
-  }else{
-    // changeIMG_2();
-  $(".back").css({
-    "background":"url('../img/info/conceptshot/concept_2.png')",
-    "background-size" : "contain",
-    "background-repeat": "no-repeat",	
-      "z-index":"-10"
-    }); 	
-  }
+  // if(scrollValue >= 380){
+  //   // changeIMG();
+  //   $(".back").css({
+  //     "background":"url('https://user-images.githubusercontent.com/96365042/209000193-0cec16cb-0ff6-41a4-b3de-757a7a502522.png')",
+  //     "background-size" : "contain",
+  //     "background-repeat": "no-repeat",
+  //     "z-index":"0"
+  //   }); 	
+  // }else{
+  //   // changeIMG_2();
+  // $(".back").css({
+  //   "background":"url('../img/info/conceptshot/concept_2.png')",
+  //   "background-size" : "contain",
+  //   "background-repeat": "no-repeat",	
+  //     "z-index":"-10"
+  //   }); 	
+  // }
 
     
 
@@ -68,7 +68,7 @@ $(window).scroll(function (e) {
 function parallax() {
   var scrolled = $(window).scrollTop();
   $(".back").css("background-position-y", (scrolled * 0.8) + "px");
-  $(".back2").css("background-position-y", (scrolled * 0.8) + "px");
+  // $(".back2").css("background-position-y", (scrolled * 0.8) + "px");
 }
 
 // 스크롤 시 백그라운드 이미지 경로 바꾸기
